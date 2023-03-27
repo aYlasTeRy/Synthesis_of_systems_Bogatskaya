c65e548e993b2c3b27fcc5eda5c42ec943ea7112
aYlasTeRy <125402146+aYlasTeRy@users.noreply.github.com>
2023-03-27 22:33:16 +0300

    Update Num_11.py

--- HW_4/Num_11.py
+++ HW_4/Num_11.py
@@ -7,13 +7,27 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        current = head
-        s = ''
-        while current:
-            s += str(current.val)
-            current = current.next
-        flag = False
-        if s == s[::-1]:
-            flag = True
 
-        return flag
+        if not head:
+            return True
+        prev = None
+
+        cur1 = head
+        cur2 = head.next
+
+        while cur2 and cur2.next:
+            cur1 = cur1.next
+            cur2 = cur2.next.next
+
+        while cur1:
+            nxt = cur1.next
+            cur1.next = prev
+            prev = cur1
+            cur1 = nxt
+
+        while prev and head:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        return True
