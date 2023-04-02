f6978f03d7393d921f92fe85918047522e1cdb29
aYlasTeRy <125402146+aYlasTeRy@users.noreply.github.com>
2023-04-03 00:56:47 +0300

    Add files via upload

---
+++ HW_5/3.3/G.py
@@ -0,0 +1,3 @@
+# https://new.contest.yandex.ru/41239/problem?id=149944/2022_10_21/i3ppbkQLTQ
+
+{element: [item for item in range(1, element + 1) if element % item == 0] for element in numbers}
---
+++ HW_5/3.3/I.py
@@ -0,0 +1,3 @@
+# https://new.contest.yandex.ru/41239/problem?id=149944/2022_10_21/hhsmfRisV6
+
+' - '.join(sorted([elements for elements in (set(str(item) for item in numbers))], key=lambda x: int(x)))
